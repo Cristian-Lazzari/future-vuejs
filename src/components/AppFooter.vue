8eaa7bd5b7bf8d59bb220f65b44ec5c198ab7cc7
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-09-24 02:09:56 +0200

    klklk

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -25,17 +25,21 @@ export default{
    @use '../assets/styles/general.scss' as *;
  
     .footer{
+        z-index: 1300;
         position: fixed;
         bottom: 0;
         left: 0;
         right: 0;
         color: white;
-        background: linear-gradient(rgba(1, 1, 1, 0),rgb(14, 6, 53));
+        background: linear-gradient(rgba(1, 1, 1, 0)20%,rgb(21, 4, 54));
         font-family: 'Roboto Condensed', sans-serif;
         display: flex;
         align-content: center;
         justify-content: space-between;
         padding: 2rem;
+        .p-i{
+            text-transform: uppercase;
+        }
     }
 
 
