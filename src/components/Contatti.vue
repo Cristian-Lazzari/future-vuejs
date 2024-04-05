4bb825ef4283595b84471d6169c5d3478d4ec9d5
DavideFarci <yank92.df@gmail.com>
2024-04-05 17:06:02 +0200

    aggiunto loader

--- src/components/Contatti.vue
+++ src/components/Contatti.vue
@@ -2,8 +2,9 @@
 import axios from "axios";
 import { store } from "../store";
 import { form_validations } from "../utilities/formValidation";
+import AppLoader from "./AppLoader.vue";
 export default {
-  components: {},
+  components: { AppLoader },
   data() {
     return {
       store,
@@ -17,7 +18,7 @@ export default {
       },
       isValid: [],
       loader: false,
-      error: false,
+      message: "",
     };
   },
   methods: {
@@ -35,9 +36,16 @@ export default {
           this.formValues
         );
         if (!message) {
+          this.message = "Impossibile inviare il messaggio";
           throw new Error("Impossibile inviare il messaggio");
         }
+
         this.loader = false;
+        this.message = "Messaggio inviato";
+        setTimeout(() => {
+          this.message = "";
+        }, 2500);
+
         this.formValues = {
           firstName: "",
           lastName: "",
@@ -148,26 +156,20 @@ export default {
         </div>
       </div>
     </form>
-    <button v-if="!loader" @click="sendMessage" class="sendMessage">
-      Invia
-    </button>
-    <button v-else class="sendMessage">
-      <div class="dot-spinner">
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-        <div class="dot-spinner__dot"></div>
-      </div>
-    </button>
+    <button @click="sendMessage" class="sendMessage">Invia</button>
     <div class="container">
       <!-- <div class="contact">mail</div>
       <span>futureplus.commerciale@gmail.com</span> -->
       <div class="contact">telefono</div>
       <span><a href="tel:+393271622244">+39 3271622244</a></span>
+    </div>
+
+    <!-- loader -->
+    <div v-if="loader || message" class="loader_overlay">
+      <div class="loader_container">
+        <AppLoader v-if="loader" :show="loader" />
+        <div v-else>{{ message }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -312,113 +314,26 @@ export default {
       }
     }
   }
-}
 
-// Loader
-.dot-spinner {
-  --uib-size: 2.8rem;
-  --uib-speed: 0.9s;
-  --uib-color: #183153;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: var(--uib-size);
-  width: var(--uib-size);
-}
+  .loader_overlay {
+    background: rgba(0, 0, 0, 0.726);
+    position: fixed;
+    inset: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    z-index: 1000;
 
-.dot-spinner__dot {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: 100%;
-  width: 100%;
-}
-
-.dot-spinner__dot::before {
-  content: "";
-  height: 20%;
-  width: 20%;
-  border-radius: 50%;
-  background-color: var(--uib-color);
-  transform: scale(0);
-  opacity: 0.5;
-  animation: pulse0112 calc(var(--uib-speed) * 1.111) ease-in-out infinite;
-  box-shadow: 0 0 20px rgba(18, 31, 53, 0.3);
-}
-
-.dot-spinner__dot:nth-child(2) {
-  transform: rotate(45deg);
-}
-
-.dot-spinner__dot:nth-child(2)::before {
-  animation-delay: calc(var(--uib-speed) * -0.875);
-}
-
-.dot-spinner__dot:nth-child(3) {
-  transform: rotate(90deg);
-}
-
-.dot-spinner__dot:nth-child(3)::before {
-  animation-delay: calc(var(--uib-speed) * -0.75);
-}
-
-.dot-spinner__dot:nth-child(4) {
-  transform: rotate(135deg);
-}
-
-.dot-spinner__dot:nth-child(4)::before {
-  animation-delay: calc(var(--uib-speed) * -0.625);
-}
-
-.dot-spinner__dot:nth-child(5) {
-  transform: rotate(180deg);
-}
-
-.dot-spinner__dot:nth-child(5)::before {
-  animation-delay: calc(var(--uib-speed) * -0.5);
-}
-
-.dot-spinner__dot:nth-child(6) {
-  transform: rotate(225deg);
-}
-
-.dot-spinner__dot:nth-child(6)::before {
-  animation-delay: calc(var(--uib-speed) * -0.375);
-}
-
-.dot-spinner__dot:nth-child(7) {
-  transform: rotate(270deg);
-}
-
-.dot-spinner__dot:nth-child(7)::before {
-  animation-delay: calc(var(--uib-speed) * -0.25);
-}
-
-.dot-spinner__dot:nth-child(8) {
-  transform: rotate(315deg);
-}
-
-.dot-spinner__dot:nth-child(8)::before {
-  animation-delay: calc(var(--uib-speed) * -0.125);
-}
-
-@keyframes pulse0112 {
-  0%,
-  100% {
-    transform: scale(0);
-    opacity: 0.5;
-  }
-
-  50% {
-    transform: scale(1);
-    opacity: 1;
+    .loader_container {
+      color: white;
+      background-color: black;
+      padding: 2rem 4rem;
+      border: 1px solid white;
+      border-radius: 10px;
+    }
   }
 }
-// Fine loader - - - -
 
 input:-webkit-autofill,
 input:-webkit-autofill:hover,
