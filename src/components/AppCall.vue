364323917a9818d6e27238cc9d1a7d69f29a50f0
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-11-15 12:58:32 +0100

    asd

--- src/components/AppCall.vue
+++ src/components/AppCall.vue
@@ -20,6 +20,7 @@ export default {
 
 <template>
   <div class="container">
+    <h2>Prenota una chiamata con in nostri <span class="c2">ESPERTI</span> </h2>
     <!-- Calendly inline widget begin -->
     <div class="calendly-inline-widget mycl" data-url="https://calendly.com/futureplus-commerciale/30min?hide_event_type_details=1&hide_gdpr_banner=1"></div>
 <!-- Calendly inline widget end -->
@@ -29,11 +30,24 @@ export default {
 <style lang="scss" scoped>
    @use '../assets/styles/general.scss' as *;
    .container{
-    padding: 1rem;
-    height: 100svh;
+    @include mmw;
+
+     height: 100svh;
     .mycl{
       height: 50svh;
     }
+    h2{
+      
+      font-size: $fs_xxlg;
+      font-weight: 600;
+      color: $c1;
+    }
+  }
+  @media (max-width: $bp_sm) {
+    .mycl{
+      height: 80svh;
+    }
+    
    }
 
 </style>
