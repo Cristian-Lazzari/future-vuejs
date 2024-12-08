5b61020795b0276921f2d96d8a0128f6f0f06489
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-12-08 16:36:09 +0100

    wer

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { reactive } from "vue";
 export const store = reactive({
   //baseUrl: "http://127.0.0.1:8000/",
   baseUrl: "https://dashboard.future-plus.it/",
-  domain: "http://localhost:5174/",
+  //domain: "http://localhost:5174/",
   domain: "https://future-plus.it/",
   nav_menu: 0,
   package : 0,
